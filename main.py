007deb4d162496686cd791c465190e975bd26fbe
Mahesh Vaidya <forvaidya@gmail.com>
2023-05-28 00:06:58 +0000

    Test.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """
+    Webserver entry point
+    """
+    return "Hello, World! - vaidya -- "
+    
+if __name__ == "__main__":
+    app.run(debug=True)
